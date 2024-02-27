477f59c445bbaaee1fdf2258c418c7dae382faf2
Chandu Tukee <141546099+chandutukee@users.noreply.github.com>
2024-02-27 19:10:33 +0530

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TechLux Hub - About Us</title>
+  <?php include "header.php"; ?>
+</head>
+<body>
+  <h1 class="page-title grid background-overlay" style="margin:1px;padding-top: 140px;padding-bottom: 140px;">About Us</h1>
+  <div class="grid" style="flex-direction:column">
+    <div style="margin:0px;"><h2 class="white-text bold" style="padding-bottom:5px;border-bottom:3px solid red;">About TechLux Hub</h2></div>
+    <div class="grey-text text-lighten-4">
+      <p style="margin:0px 150px 0px 150px;font-size:25px;text-align:justify">
+        TechLux Hub was a Company that selling Computer Component which found by four university student and started its business at Karnataka on 1th January 2024.
+        Our passion towards computers makes us willing to introduce and provide the most advanced and recommeded products to our customers.
+        Besides selling Computer Component, our staff will also provide build guides for those who was new to build their PC. 
+        We will recommend and help you build your perfect PC according to your budget.
+        TechLux Hub also continuously update prices from dozens of the most popular online retailers to help you to save your money.
+      </p>
+    </div>
+    <div style="margin:0px;"><h2 class="white-text bold" style="padding-bottom:5px;border-bottom:3px solid red;">History of TechLux Hub</h2></div>
+    <div class="grey-text text-lighten-4">
+      <p style="margin:0px 150px 0px 150px;font-size:25px;text-align:justify">
+      Early, TechLux Hub was only a manual store without website and database system.
+      Besides, records the information manually using file system was been elaminated in this advanced age. 
+      Therefore, TechLux Hub Company completely modernize the system from manual system to online system which is a web application system. 
+      However, TechLux Hub also reserved the old method of selling, which is selling at store, so the customers have two choices of methods to purchase our product. 
+      </p>
+    </div>
+  </div>
+  <?php require "footer.php"; ?>
+</body>
+</html>
